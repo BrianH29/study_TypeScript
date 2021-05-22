73e62d5806d60e6ae4aae04c33b885ef4fbe1c96
brian <brianhwang29@gmail.com>
2021-05-22 22:31:04 +0900

    optional, default, rest parameter

--- 1-TYPES/1-2-function.ts
+++ 1-TYPES/1-2-function.ts
@@ -25,10 +25,37 @@
         //code...
         //code...
         //code...
-        
+
         return new Promise((resolve, reject) => {
             resolve(100); 
         })
     }
+
+    //JavaScript => TypeScript
+    //Optional parameter - '?' with questionmark it gives the option of parameter 
+    function printName(firstName: string, lastName?: string){
+        console.log(firstName);
+        console.log(lastName); // 전달 값이 없으면 undefined
+    }
+    printName('Steve','Jobs');
+    printName('Brian'); 
+
+    //Default parameter
+    function printMessage(message:string = 'default message'){
+        console.log(message); 
+    }
+    printMessage(); 
+
+    //Rest parameter
+    function addNumbers(...numbers:number[]):number{
+        return numbers.reduce((a,b) => a+b)
+    }
+
+    console.log(addNumbers(1,2));
+    console.log(addNumbers(1,2,3,4));
+
+
+
+
 
 }
