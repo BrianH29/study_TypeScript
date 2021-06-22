b9199780cbcc08e6a79a6fec52effeb8d7bddbf7
BrianH <brianhwang29@gmail.com>
2021-06-22 13:02:59 +0900

    coffee maker modified

--- 3-OOP/3-2-class.ts
+++ 3-OOP/3-2-class.ts
@@ -5,19 +5,26 @@
   }
 
   class CoffeeMaker {
-    BEANS_GRAMM_PER_SHOT:number = 7  // class level - static을 붙여서 생성 할 때 마다 생성되지 않게 하기 위해서 
+    static BEANS_GRAMM_PER_SHOT:number = 7  // class level - static을 붙여서 생성 할 때 마다 생성되지 않게 하기 위해서 
     coffeeBeans : number = 0; // instance (object) level
 
     constructor(coffeeBeans: number){
       this.coffeeBeans = coffeeBeans; 
     }
 
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans); 
+    }
+    /**
+     * BEANS_GRAMM_PER_SHOT : 생성 될때 마다 똑같은 값이므로 static을 붙여 줬을때 this.BEANS.. 가 아닌 class 이름을 붙인다. 
+     * this.BEANS.. ==> CoffeeMaker.BEANS...
+     */
     makeCoffee(shots: number): CoffeeCup {
-      if(this.coffeeBeans < shots * this.BEANS_GRAMM_PER_SHOT){
+      if(this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT){
         throw new Error('not enough coffee beans!')
       }
 
-      this.coffeeBeans -= shots * this.BEANS_GRAMM_PER_SHOT;
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
       return {
         shots,
         hasMilk: false,
@@ -29,4 +36,7 @@
   const maker2 = new CoffeeMaker(15); 
   console.log(maker); 
   console.log(maker2); 
+
+  const maker3 = CoffeeMaker.makeMachine(3);
+  console.log(maker3); 
 }
