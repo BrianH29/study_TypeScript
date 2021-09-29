c402ec999eb17bc564bff9de5e52583209bfe3f7
BrianH <brianhwang29@gmail.com>
2021-09-29 15:45:43 +0900

    modified

--- 3-OOP/3-2-class.ts
+++ 3-OOP/3-2-class.ts
@@ -1,42 +1,34 @@
 {
   type CoffeeCup = {
     shots: number;
-    hasMilk: boolean;
+    milk : boolean;
   }
 
+  //class 안에서는 const,let,function등 타입을 선언하지 않는다. 
   class CoffeeMaker {
-    static BEANS_GRAMM_PER_SHOT:number = 7  // class level - static을 붙여서 생성 할 때 마다 생성되지 않게 하기 위해서 
-    coffeeBeans : number = 0; // instance (object) level
+    BEANS_GRAM_PER_SHOT:number = 7; 
+    coffeeBeans:number = 0; 
 
-    constructor(coffeeBeans: number){
-      this.coffeeBeans = coffeeBeans; 
+    //클래스를 가지고 오브젝트 인스턴스를 생성할 때 호출되는 함수 
+    constructor(coffeeBeans:number){
+      this.coffeeBeans = coffeeBeans;
     }
 
-    static makeMachine(coffeeBeans: number): CoffeeMaker {
-      return new CoffeeMaker(coffeeBeans); 
-    }
-    /**
-     * BEANS_GRAMM_PER_SHOT : 생성 될때 마다 똑같은 값이므로 static을 붙여 줬을때 this.BEANS.. 가 아닌 class 이름을 붙인다. 
-     * this.BEANS.. ==> CoffeeMaker.BEANS...
-     */
-    makeCoffee(shots: number): CoffeeCup {
-      if(this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT){
-        throw new Error('not enough coffee beans!')
+    makeCoffee(shots:number):CoffeeCup{
+      if(this.coffeeBeans < shots * this.BEANS_GRAM_PER_SHOT){
+        throw new Error('not enough coffee beans')
       }
-
-      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+  
+      this.coffeeBeans -= shots * this.BEANS_GRAM_PER_SHOT;
+  
       return {
         shots,
-        hasMilk: false,
+        milk: false,
       }
     }
+
   }
 
   const maker = new CoffeeMaker(32); 
-  const maker2 = new CoffeeMaker(15); 
   console.log(maker); 
-  console.log(maker2); 
-
-  const maker3 = CoffeeMaker.makeMachine(3);
-  console.log(maker3); 
 }
