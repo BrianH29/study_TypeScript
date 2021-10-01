e71c61f9ba3385892a2827fa10deacab8ae118fc
BrianH <brianhwang29@gmail.com>
2021-10-01 15:47:47 +0900

    modified

--- 7-advancedType/7-2-index.ts
+++ 7-advancedType/7-2-index.ts
@@ -18,4 +18,21 @@
   type Gender = Animal['gender']; //'male' | 'female'
   //const lion:Gender = 'trans'
   const lion:Gender = 'male'; 
+
+  /**
+   * keyof
+   * -> Animal 에 있는 모든 키의 타입을 할당 하는 것. 
+   */
+  type Keys = keyof Animal; // 'name' | 'age' | 'gender'
+  const key: Keys = 'gender'
+
+  type Person = {
+    name:string;
+    gender:Animal['gender']
+  };
+
+  const person:Person ={
+    name: 'bi',
+    gender: 'female'
+  }
 }
