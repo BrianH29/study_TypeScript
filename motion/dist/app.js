d5edf9a2d461a1a5cae296838a74ea6e08117f44
BrianH <brianhwang29@gmail.com>
2022-01-06 16:29:37 +0900

    components

--- motion/dist/app.js
+++ motion/dist/app.js
@@ -1,11 +1,21 @@
 import { PageComponent } from "./components/page/page.js";
 import { ImageComponent } from "./components/page/item/image.js";
+import { NoteComponent } from "./components/page/item/note.js";
+import { TodoComponent } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
         image.attachTo(appRoot, "beforeend");
+        const note = new NoteComponent('Note Title', 'Note BODY');
+        note.attachTo(appRoot, "beforeend");
+        const todo = new TodoComponent('ToDo Title', 'Todo Item');
+        todo.attachTo(appRoot, 'beforeend');
+        const url = 'https://www.youtube.com/watch?v=TTLHd3IyErM';
+        const video = new VideoComponent(url, 'Video TITLE');
+        video.attachTo(appRoot, 'beforeend');
     }
 }
 const el = document.querySelector('.document');
--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,5 +1,8 @@
 import {PageComponent} from "./components/page/page.js";
 import {ImageComponent} from "./components/page/item/image.js";
+import {NoteComponent} from "./components/page/item/note.js";
+import {TodoComponent} from "./components/page/item/todo.js";
+import {VideoComponent} from "./components/page/item/video.js";
 
 class App{
     private readonly page: PageComponent;
@@ -10,6 +13,16 @@ class App{
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
         image.attachTo(appRoot, "beforeend")
+
+        const note = new NoteComponent('Note Title', 'Note BODY');
+        note.attachTo(appRoot, "beforeend");
+
+        const todo = new TodoComponent('ToDo Title','Todo Item');
+        todo.attachTo(appRoot, 'beforeend')
+
+        const url = 'https://www.youtube.com/watch?v=TTLHd3IyErM'
+        const video = new VideoComponent(url, 'Video TITLE');
+        video.attachTo(appRoot, 'beforeend');
     }
 }
 
